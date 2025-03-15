fab20f2f21863b43943a3bad8c7ea92053fa3394
ValerianArdelean <valerian_ardelean@yahoo.com>
2025-03-15 14:57:21 +0200

    merged gameOver and UpdateDinoDisplay functions in to one

--- script.js
+++ script.js
@@ -4,15 +4,74 @@ const START_GRID = 10;
 const GROUND_FLOOR = 32;
 const GRID_HEIGHT = 34;
 const GRID_WIDTH = 74;
+const DINO_POS = 22;
+const DINO_SHAPE = [
+	[0, 1], [0, 2], [1, 1], [1, 2], [2, 1],
+	[2, -1], [3, -1], [3, 0], [3, 1], [3, 2], [4, 1], [4, 0], [4,-1],
+	[5, 1], [5, -1]
+];
+const CACTUS_SHAPE = [[], [
+	[0, 0], [1, 0], [2, 0], [3, 0], [4, 0], [5, 0],
+	[2, 1], [2, 2], [3, 2],
+	[1, -1], [1, -2], [2, -2], [3, -2]
+], [
+	[0, 0], [1, 0], [2, 0], [3, 0], [4, 0],
+	[2, 1], [2, 2], [3, 2],
+	[1, -1], [1, -2], [2, -2],
+
+	[0, 4], [1, 4], [2, 4], [3, 4], [4, 4], [5, 4],
+	[2, 5], [2, 6], [3, 6],
+	[1, 3], [1, 2]
+]];
+const GAME_OVER_SHAPE = [
+	// G
+	[18, 18], [18, 19], [18, 20], [18, 21], [18, 22], [18, 22],
+	[19, 18], [20, 18], [21, 18], [22, 18],
+	[22, 19], [22, 20], [22, 21], [22, 22],
+	[21, 22], [20, 22],
+	[20, 20], [20, 21], [20, 22],
+	// A
+	[18, 26], [19, 25], [19, 27], [20, 24], [20, 28],
+	[20, 24], [20, 25], [20, 26], [20, 27], [20, 28],
+	[21, 24], [22, 24], [21, 28], [22, 28],
+	// M
+	[18, 30], [19, 30], [20, 30], [21, 30], [22, 30],
+	[20, 32], [19, 31],
+	[19, 33],
+	[18, 34], [19, 34], [20, 34], [21, 34], [22, 34],
+	// E
+	[18, 36], [18, 37], [18, 38], [18, 39], [18, 40],
+	[19, 36], [20, 36], [21, 36], [22, 36],
+	[20, 37], [20, 38],
+	[22, 36], [22, 37], [22, 38], [22, 39], [22, 40],
+	// O
+	[18, 46], [18, 45], [18, 47], [18, 44], [20, 43], [19, 43],
+	[21, 43], [19, 48], [20, 48], [21, 48],
+	[22, 46], [22, 45], [22, 44], [22, 47],
+	// V
+	[22, 52], [21, 51], [20, 50],
+	[21, 53], [20, 54],
+	[19, 50], [19, 54], [18, 50], [18, 54],
+	// E
+	[18, 56], [18, 57], [18, 58], [18, 59], [18, 60],
+	[19, 56], [20, 56], [21, 56], [22, 56],
+	[20, 57], [20, 58],
+	[22, 56], [22, 57], [22, 58], [22, 59], [22, 60],
+	// R
+	[18, 63], [19, 62], [20, 62], [21, 62], [22, 62],
+	[18, 63], [18, 64], [18, 65], [19, 66],
+	[20, 63], [20, 64], [20, 65],
+	[21, 65], [22, 66],
+];
 
 let game = {
 	grid : document.getElementById("game"),
-	dinoCoordinates : [28, 22],
-	jumps : 0,
 	scoreElement : document.getElementById("score"),
-	time : {interval : 0, elapsed : 0, minutes : 0, seconds : 0},
+	dinoHeight : 28,
+	jumps : 0,
 	gameOver : false,
-	gameRunning : false
+	gameRunning : false,
+	time : {interval : 0, elapsed : 0, minutes : 0, seconds : 0}
 }
 
 function handleButton(button) {
@@ -60,9 +119,9 @@ function createGround() {
 }
 
 function updateCoordinates(sign) {
-	let newHeight = game.dinoCoordinates[0] + sign;
+	let newHeight = game.dinoHeight + sign;
 	if (newHeight > MIN_DINO_HEIGHT && newHeight <= MAX_DINO_HEIGHT) {
-		game.dinoCoordinates[0] = newHeight;
+		game.dinoHeight = newHeight;
 	}
 }
 
@@ -70,86 +129,19 @@ function selectCell(line, col) {
 	return document.getElementById(`${line}${col}`);
 }
 
-function updateDinoDisplay(status) {
-	const DINO_SHAPE = [
-		[0, 1], [0, 2], [1, 1], [1, 2], [2, 1],
-		[2, -1], [3, -1], [3, 0], [3, 1], [3, 2], [4, 1], [4, 0], [4,-1],
-		[5, 1], [5, -1]
-	];
-	
-	DINO_SHAPE.forEach(([x, y]) => {
-		selectCell(game.dinoCoordinates[0] + x, game.dinoCoordinates[1] + y).classList[status]("gray");
+function toogleCells(status, shape, line, col) {
+	shape.forEach(([x, y]) => {
+		selectCell(line + x, col + y).classList[status]("gray");
 	});
 }
 
-function gameOver() {
-	const GAME_OVER_SHAPE = [
-		// G
-		[18, 18], [18, 19], [18, 20], [18, 21], [18, 22], [18, 22],
-		[19, 18], [20, 18], [21, 18], [22, 18],
-		[22, 19], [22, 20], [22, 21], [22, 22],
-		[21, 22], [20, 22],
-		[20, 20], [20, 21], [20, 22],
-		// A
-		[18, 26], [19, 25], [19, 27], [20, 24], [20, 28],
-		[20, 24], [20, 25], [20, 26], [20, 27], [20, 28],
-		[21, 24], [22, 24], [21, 28], [22, 28],
-		// M
-		[18, 30], [19, 30], [20, 30], [21, 30], [22, 30],
-		[20, 32], [19, 31],
-		[19, 33],
-		[18, 34], [19, 34], [20, 34], [21, 34], [22, 34],
-		// E
-		[18, 36], [18, 37], [18, 38], [18, 39], [18, 40],
-		[19, 36], [20, 36], [21, 36], [22, 36],
-		[20, 37], [20, 38],
-		[22, 36], [22, 37], [22, 38], [22, 39], [22, 40],
-		// O
-		[18, 46], [18, 45], [18, 47], [18, 44], [20, 43], [19, 43],
-		[21, 43], [19, 48], [20, 48], [21, 48],
-		[22, 46], [22, 45], [22, 44], [22, 47],
-		// V
-		[22, 52], [21, 51], [20, 50],
-		[21, 53], [20, 54],
-		[19, 50], [19, 54], [18, 50], [18, 54],
-		// E
-		[18, 56], [18, 57], [18, 58], [18, 59], [18, 60],
-		[19, 56], [20, 56], [21, 56], [22, 56],
-		[20, 57], [20, 58],
-		[22, 56], [22, 57], [22, 58], [22, 59], [22, 60],
-		// R
-		[18, 63], [19, 62], [20, 62], [21, 62], [22, 62],
-		[18, 63], [18, 64], [18, 65], [19, 66],
-		[20, 63], [20, 64], [20, 65],
-		[21, 65], [22, 66],
-	];
-
-	GAME_OVER_SHAPE.forEach(([x, y]) => {
-		selectCell(x, y).classList.add("gray");
-	});
-}
-
-function updateCactusDisplay(column, status, type) {
-	const CACTUS_SHAPE = [[], [
-		[0, 0], [1, 0], [2, 0], [3, 0], [4, 0], [5, 0],
-		[2, 1], [2, 2], [3, 2],
-		[1, -1], [1, -2], [2, -2], [3, -2]
-	], [
-		[0, 0], [1, 0], [2, 0], [3, 0], [4, 0],
-		[2, 1], [2, 2], [3, 2],
-		[1, -1], [1, -2], [2, -2],
-
-		[0, 4], [1, 4], [2, 4], [3, 4], [4, 4], [5, 4],
-		[2, 5], [2, 6], [3, 6],
-		[1, 3], [1, 2]
-	]];
-
-	CACTUS_SHAPE[type].forEach(([x, y]) => {
+function updateCactusDisplay(column, status, type, shape) {
+	shape[type].forEach(([x, y]) => {
 		if (column - y > START_GRID) {
 			let cell = selectCell(GROUND_FLOOR - x, column - y);
 			if (cell.classList.contains("gray") && status == "add") {
 				game.gameOver = true;
-				gameOver();
+				toogleCells("add", GAME_OVER_SHAPE, 0, 0);
 				clearInterval(game.time.interval);
 				if (game.scoreElement.innerText > document.getElementById("HighestScore").innerText) {
 					document.getElementById("HighestScore").innerText = game.scoreElement.innerText;
@@ -160,14 +152,14 @@ function updateCactusDisplay(column, status, type) {
 	});
 }
 
-function displayCactuses() {
+function displayCactus() {
 	const SPEED = 30;
 	let noCactuses = Math.floor(Math.random() * 2) + 1;
 	let cactusPos = GRID_WIDTH - 2;
 	let cactus = setInterval(function () {
-		updateCactusDisplay(cactusPos, "remove", noCactuses);
+		updateCactusDisplay(cactusPos, "remove", noCactuses, CACTUS_SHAPE);
 		--cactusPos;
-		updateCactusDisplay(cactusPos, "add", noCactuses);
+		updateCactusDisplay(cactusPos, "add", noCactuses, CACTUS_SHAPE);
 		if (game.gameOver) {
 			clearInterval(cactus);
 		}
@@ -179,7 +171,7 @@ function startGame() {
 	if (!game.gameRunning) {
 		game.gameRunning = true;
 		let cactuses = setInterval(function () {
-			displayCactuses();
+			displayCactus();
 			if (game.gameOver) {
 				clearInterval(cactuses);
 			}
@@ -209,9 +201,9 @@ function jumpDino() {
 	function startTimer(speed, steps, sign) {
 		let step = 0;
 		let timer = setInterval(function() {
-			updateDinoDisplay("remove");
+			toogleCells("remove", DINO_SHAPE, game.dinoHeight, DINO_POS);
 			updateCoordinates(sign);
-			updateDinoDisplay("add");
+			toogleCells("add", DINO_SHAPE, game.dinoHeight, DINO_POS);
 			++step;
 			if (step == steps) {
 				clearInterval(timer);
@@ -222,7 +214,7 @@ function jumpDino() {
 				}
 				startTimer(VELOCITY[height][SPEED], VELOCITY[height][TIME], -direction);
 			}
-			if (game.dinoCoordinates[0] == MAX_DINO_HEIGHT - 1 && direction == -1) {
+			if (game.dinoHeight == MAX_DINO_HEIGHT - 1 && direction == -1) {
 				game.jumps = 0;
 			}
 		}, speed);
@@ -234,7 +226,7 @@ function restart() {
 	game.grid.innerHTML = "";
 	createCells();
 	createGround();
-	updateDinoDisplay("add");
+	toogleCells("add", DINO_SHAPE, game.dinoHeight, DINO_POS);
 	game.gameOver = false;
 	game.gameRunning = false;
 	game.time.minutes = 0;
@@ -243,5 +235,5 @@ function restart() {
 
 createCells();
 createGround();
-updateDinoDisplay("add");
+updateDinoDisplay("add", DINO_SHAPE);
 handleKeyboardInput();
